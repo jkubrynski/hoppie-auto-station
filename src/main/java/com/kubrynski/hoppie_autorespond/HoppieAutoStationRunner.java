e103e2061ae2709a0c43a1655f4b3ae26ec9bf45
Jakub Kubrynski <jkubrynski@gmail.com>
2023-01-03 22:13:08 +0100

    Implement response handler

--- src/main/java/com/kubrynski/hoppie_autorespond/HoppieAutoStationRunner.java
+++ src/main/java/com/kubrynski/hoppie_autorespond/HoppieAutoStationRunner.java
@@ -3,6 +3,7 @@ package com.kubrynski.hoppie_autorespond;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +13,13 @@ public class HoppieAutoStationRunner {
     public static void main(String[] args) throws IOException {
         System.out.println("Running Hoppie Auto Station...");
         HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8080), 0);
+        httpServer.createContext("/", exchange -> {
+            String response = "OK";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream responseBody = exchange.getResponseBody();
+            responseBody.write(response.getBytes());
+            responseBody.close();
+        });
         httpServer.start();
 
         AcarsMessageProcessor acarsMessageProcessor = new AcarsMessageProcessor(
