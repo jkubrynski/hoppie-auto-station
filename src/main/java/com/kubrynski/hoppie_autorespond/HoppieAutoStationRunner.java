0fe4631c7a47a299f912b0a4a9db7862f5e1aff3
Jakub Kubrynski <jkubrynski@gmail.com>
2023-01-03 21:34:42 +0100

    Open 8080 port for docker

--- src/main/java/com/kubrynski/hoppie_autorespond/HoppieAutoStationRunner.java
+++ src/main/java/com/kubrynski/hoppie_autorespond/HoppieAutoStationRunner.java
@@ -1,11 +1,18 @@
 package com.kubrynski.hoppie_autorespond;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class HoppieAutoStationRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8080), 0);
+        httpServer.start();
+
         AcarsMessageProcessor acarsMessageProcessor = new AcarsMessageProcessor(
                 System.getenv("HOPPIE_STATION"),
                 System.getenv("HOPPIE_SECRET")
